9660814a63b6b5a8d16592d33ed7381c9e54106f
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-01 21:32:26 -0500

    Made Experience return labels and inputs for company name, position, and start and end date of employment

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,7 +6,22 @@ class Experience extends Component {
   }
 
   render() {
-    return;
+    return (
+      <>
+        <h2>Experience</h2>
+        <label htmlFor="company">Company name:</label>
+        <input id="company" type="text" name="company"></input>
+
+        <label htmlFor="position">Position title:</label>
+        <input id="position" type="text" name="position"></input>
+
+        <label htmlFor="from">From:</label>
+        <input id="from" type="tel" name="from"></input>
+
+        <label htmlFor="to">To:</label>
+        <input id="to" type="tel" name="to"></input>
+      </>
+    );
   }
 }
 
