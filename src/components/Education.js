dfd7eae1b93e5e511fd7a700795a8fa14be58039
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-01 20:33:14 -0500

    Made Education component and set up utilizing classes

---
+++ src/components/Education.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Education extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return;
+  }
+}
+
+export default Education;
