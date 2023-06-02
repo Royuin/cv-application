5e7372adeb5f7eaa81aaf07aa263cf9afb2dedb6
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-01 20:51:02 -0500

    Made Education component make label and input fields for users school, field of study and date of study

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,7 +6,19 @@ class Education extends Component {
   }
 
   render() {
-    return;
+    return (
+      <>
+        <h2>Education</h2>
+        <label htmlFor="school">School:</label>
+        <input id="school" type="text" name="school"></input>
+
+        <label htmlFor="study">Field of study:</label>
+        <input id="study" type="text" name="study"></input>
+
+        <label htmlFor="date">Date of study:</label>
+        <input id="date" type="text" name="date"></input>
+      </>
+    );
   }
 }
 
