88cc92cf22b8123bd4f6d84c99f629c1dc9013c1
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-01 19:19:18 -0500

    Made components file and file for users general info and set up class with imports and exports

---
+++ src/components/Info.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './App.js';
+
+class Info extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+   return (
+
+    );
+  }
+}
+
+export default Info;
