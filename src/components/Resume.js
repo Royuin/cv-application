725d2ea0af40018248edc0e1452e9eb9c2774b3b
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-03 22:01:38 -0500

    Made render resume method that currently only has query selectors for all of users values for all inputs

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,11 +2,25 @@ import { Component } from 'react';
 
 class Resume extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
+  renderResume = () => { 
+    const firstNameValue = document.getElementById('firstName').value;
+    const lastNameValue = document.getElementById('lastName').value;
+    const emailValue = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const school = document.getElementById('school').value;
+    const subject = document.getElementById('subject').value;
+    const date = document.getElementById('date').value;
+    const company = document.getElementById('company').value;
+    const position = document.getElementById('position').value;
+    const from = document.getElementById('from').value;
+    const to = document.getElementById('to').value;
+};
+  
   render() {
-  return;
+    return;
   }
 }
 
