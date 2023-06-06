86d615b0eb4568dd2500cd9a32b84252391950b7
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-05 21:29:18 -0500

    Added renderExperience method to render resume content of users experience

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ class Resume extends Component {
     super(props);
     this.state = this.props.state;
     this.renderGeneralInfo = this.renderGeneralInfo.bind(this);
-    this.renderResume = this.renderResume.bind(this);
+    this.renderExperience = this.renderExperience.bind(this);
   }
 
   renderGeneralInfo = () => {
@@ -18,10 +18,24 @@ class Resume extends Component {
     );
   };
 
-  renderResume = () => {};
+  renderExperience = () => {
+    return (
+      <section>
+        <h3>{this.props.state.company}</h3>
+        <h4>{this.props.state.position}</h4>
+        <p>{this.props.state.from}</p>
+        <p>{this.props.state.to}</p>
+      </section>
+    );
+  };
 
   render() {
-    return <section>{this.renderGeneralInfo()}</section>;
+    return (
+      <section>
+        {this.renderGeneralInfo()}
+        {this.renderExperience()}
+      </section>
+    );
   }
 }
 
