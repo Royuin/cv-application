bcc4274d14850f7b4fbd5b0fb6d8142149f8d150
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-20 07:49:49 -0500

    Refactored Resume.js and renderGeneralInfo to functional. Commented out functions that still need to be refactored

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,66 +1,58 @@
 import { Component } from 'react';
 import '../styles/Resume.css';
 
-class Resume extends Component {
-  constructor(props) {
-    super(props);
-    this.state = this.props.state;
-  }
+export default function Resume ({firstName, lastName, email, phone}) {
 
-  renderGeneralInfo = () => {
+  function renderGeneralInfo () {
     return (
       <section className="general-info">
         <h2 className="name">
-          {this.props.state.firstName} {this.props.state.lastName}
+          {firstName} {lastName}
         </h2>
-        <p className="email">{this.props.state.email}</p>
-        <p className="phone">{this.props.state.phone}</p>
+        <p className="email">{email}</p>
+        <p className="phone">{phone}</p>
       </section>
     );
   };
 
-  renderExperience = () => {
-    const jobItems = this.props.state.jobs.map((job) => {
-      return (
-        <li key={job.id} >
-          <div className='company-info'>
-          <h4>{job.company}</h4>
-          <h5>{job.position}</h5>
-          </div>
-          <div className='company-date'>
-          <p>From: {job.from}</p>
-          <p>To: {job.to}</p>
-          </div>
-        </li>
-      );
-    }); 
-    return <ul>{jobItems}</ul>;
+  // renderExperience = () => {
+  //   const jobItems = this.props.state.jobs.map((job) => {
+  //     return (
+  //       <li key={job.id} >
+  //         <div className='company-info'>
+  //         <h4>{job.company}</h4>
+  //         <h5>{job.position}</h5>
+  //         </div>
+  //         <div className='company-date'>
+  //         <p>From: {job.from}</p>
+  //         <p>To: {job.to}</p>
+  //         </div>
+  //       </li>
+  //     );
+  //   }); 
+  //   return <ul>{jobItems}</ul>;
+  // };
 
-  };
+  // renderEducation = () => {
+  //   return (
+  //     <section className="education">
+  //       <h3>Education:</h3>
+  //       <h4>{this.props.state.school}</h4>
+  //       <h5>{this.props.state.subject}</h5>
+  //       <p>{this.props.state.date}</p>
+  //     </section>
+  //   );
+  // };
 
-  renderEducation = () => {
-    return (
-      <section className="education">
-        <h3>Education:</h3>
-        <h4>{this.props.state.school}</h4>
-        <h5>{this.props.state.subject}</h5>
-        <p>{this.props.state.date}</p>
+  // {this.renderExperience()}
+  // {this.renderEducation()}
+
+  return (
+    <section className="resume">
+      {renderGeneralInfo()}
+      <section className='experience'>
+        <h3>Work Experience: </h3>
       </section>
-    );
-  };
-
-  render() {
-    return (
-      <section className="resume">
-        {this.renderGeneralInfo()}
-        <section className='experience'>
-          <h3>Work Experience: </h3>
-          {this.renderExperience()}
-        </section>
-        {this.renderEducation()}
-      </section>
-    );
-  }
+    </section>
+  );
 }
-
-export default Resume;
