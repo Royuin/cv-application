d9942a5ba17b1ef407216e229a2e82cb03e84a78
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-20 19:58:03 -0500

    Refactored all functions for functional App from previously being class based and passed all needed values and functions to Experience and Resume components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Experience from './components/Experience.js';
 import Resume from './components/Resume.js';
 import uniqid from 'uniqid';
 
-function App() { 
+export default function App() { 
   const [firstName, setFirstName] = useState('');   
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
@@ -15,21 +15,8 @@ function App() {
   const [subject, setSubject] = useState('');
   const [date, setDate] = useState('');
   const [jobs, setJobs] = useState([{ company: '', position: '', from: '', to: '', id: uniqid(),}])
-  // this.handleSubmit = this.handleSubmit.bind(this);
-  //   this.handleAdd = this.handleAdd.bind(this);
-  //   this.handleJobDelete = this.handleJobDelete.bind(this);
-  //   this.state = {
-  //     firstName: '',
-  //     lastName: '',
-  //     email: '',
-  //     phone: '',
-  //     school: '',
-  //     subject: '',
-  //     date: '',
-  //     jobs: [{ company: '', position: '', from: '', to: '', id: uniqid(),}],
-  //   };
 
- function displayForm() {
+  function displayForm() {
     const form = document.querySelector('form');
     const editBtn = document.querySelector('.edit-button');
     const resume = document.querySelector('.resume');
@@ -49,24 +36,22 @@ function App() {
     setPhone(phone)
   };
 
-  // setJobsState = () => {
-  //   const jobs = this.state.jobs.map((job) => {
-  //     const company = document.getElementById('company' + job.id).value;
-  //     const position = document.getElementById('position' + job.id).value;
-  //     const from = document.getElementById('from' + job.id).value;
-  //     const to = document.getElementById('to' + job.id).value;
-  //     return {
-  //       company,
-  //       position,
-  //       from,
-  //       to,
-  //       id: job.id
-  //     };
-  //   });
-  //   this.setState({
-  //     jobs: jobs,
-  //   });
-  // };
+  function setJobsState() {
+    const updatedJobs = jobs.map((job) => {
+      const company = document.getElementById('company' + job.id).value;
+      const position = document.getElementById('position' + job.id).value;
+      const from = document.getElementById('from' + job.id).value;
+      const to = document.getElementById('to' + job.id).value;
+      return {
+        company,
+        position,
+        from,
+        to,
+        id: job.id
+      };
+    });
+    setJobs(updatedJobs);
+  };
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -76,10 +61,8 @@ function App() {
     setSchool(school);
     setSubject(subject);
     setDate(date);
-    
     setInfoState();
-    // this.setJobsState();
-
+    setJobsState();
     const form = document.querySelector('form');
     const editBtn = document.querySelector('.edit-button');
     const resume = document.querySelector('.resume');
@@ -88,32 +71,21 @@ function App() {
     resume.style.display = 'grid';
   };
 
-  // handleAdd = () => {
-  //   const job = {
-  //     company: '',
-  //     position: '',
-  //     from: '',
-  //     to: '',
-  //     id: uniqid(),
-  //   };
-  //   this.setState({
-  //     jobs: this.state.jobs.concat(job),
-  //   });
-  // };
+  function  handleAdd() {
+    const job = {
+      company: '',
+      position: '',
+      from: '',
+      to: '',
+      id: uniqid(),
+    };
+    setJobs(previous => [...previous, job])
+  };
 
-  // handleJobDelete = (id) => {
-  //   const updatedJobs = this.state.jobs.filter((job) => job.id !== id);
-  //   this.setState({
-  //     jobs: updatedJobs,
-  //   });
-  // };
-
-  // <Education  />
-  // <Experience
-  // />
-  // <button type="button">
-  //   Add Experience
-  // </button>
+  function handleJobDelete(id) {
+    const updatedJobs = jobs.filter((job) => job.id !== id);
+    setJobs(updatedJobs)
+  };
 
   return (
     <div className="App">
@@ -121,6 +93,17 @@ function App() {
       <form>
         <Info />
         <Education />
+        <Experience 
+          jobs={jobs}
+          handleAdd={handleAdd}
+          handleJobDelete={handleJobDelete}
+        />
+        <button 
+          type='button'
+          onClick={handleAdd}
+        >
+          Add
+        </button>
         <button
           type="submit"
           onClick={(e) => {
@@ -129,22 +112,20 @@ function App() {
         >
           Create
         </button>
-     </form>
-         <Resume 
-          firstName={firstName}
-          lastName={lastName}
-          email={email}
-          phone={phone}
-          school={school}
-          subject={subject}
-          date={date}
-        />
-  <button className="edit-button" onClick={displayForm}>
-    Edit
-  </button>
+      </form>
+      <Resume 
+        firstName={firstName}
+        lastName={lastName}
+        email={email}
+        phone={phone}
+        school={school}
+        subject={subject}
+        date={date}
+        jobs={jobs}
+      />
+      <button className="edit-button" onClick={displayForm}>
+        Edit
+      </button>
     </div>
   );
-
 }
-
-export default App;
